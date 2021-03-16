85531250490ad84858fbc2da8164f58b320cc0a3
MohannedM <mohanned.mohammed@msa.edu.eg>
2021-03-16 15:20:38 +0200

    config change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:3000"
+        origins "https://flight-planner-fe.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Set-Cookie']
     end
 
     allow do
-        origins "https://flight-planner-fe.herokuapp.com/"
+        origins "http://localhost:3000"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Set-Cookie']
     end
 end
